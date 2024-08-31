956b1fd060ba5f94cb87e725551df5d0fd3c22fb
Jared Hoyt <jhoyt2@huskers.unl.edu>
2024-08-31 11:51:17 -0500

    Remove `permute` function.

--- services/des.py
+++ services/des.py
@@ -3,17 +3,13 @@ import numpy as np
 import numpy.typing as npt
 
 
-def permute(permutation_table, bit_string):
-    return bit_string[permutation_table - 1]
-
-
 def encrypt(text, subkeys: npt.NDArray[np.uint8]):
-    permuted_message = permute(constants.IP, text)
-    l = permuted_message[:32]
-    r = permuted_message[32:]
+    permutation = text[constants.IP - 1]
+    l = permutation[:32]
+    r = permutation[32:]
 
     for subkey in subkeys:
-        xor = subkey ^ permute(constants.E_BIT, r)
+        xor = subkey ^ r[constants.E_BIT - 1]
         result = np.empty((0,), dtype=np.uint8)
 
         for i in range(8):
@@ -24,7 +20,7 @@ def encrypt(text, subkeys: npt.NDArray[np.uint8]):
             s_box_value = constants.S_BOXES[i][row][col]
             result = np.concatenate((result, constants.S_BOX_CONVERSION[s_box_value]))
 
-        f = permute(constants.P, result)
+        f = result[constants.P - 1]
         l, r = r, l ^ f
 
-    return permute(constants.IP_I, np.concatenate((r, l)))
+    return np.concatenate((r, l))[constants.IP_I - 1]
